0a44ce99150af1d7fa9d840e9d764e978685d0c2
Vinicius C Scampini <viniciuscscampini@gmail.com>
2024-08-07 10:16:41 -0300

    Adicionando ciclo no observable

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,22 +7,28 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
 
   campoBusca:string = '';
+
+  subscription:Subscription;
 
   constructor(
     private livroService : LivroService
   ) { }
 
   buscarLivros() {
-    this.livroService.buscar(this.campoBusca).subscribe({
+    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
       next: (data) => console.log(data),
       error: (error) => console.error(error),
       complete: () => console.info('Deu') 
     })
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
   
 }
