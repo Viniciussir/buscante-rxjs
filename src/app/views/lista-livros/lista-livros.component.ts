915bb1e4421b7765eb3372387d000abee1b3ed4d
Vinicius C Scampini <viniciuscscampini@gmail.com>
2024-08-07 12:20:31 -0300

    otimizando a busca com o RxJS utilizando o filter, swtichmap e debounceTime

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,45 +1,38 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { Subscription, debounce, debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
 import { Item, Livro } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
+const PAUSA = 1000;
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy {
+export class ListaLivrosComponent {
 
-  listaLivros: Livro[];
-
-  campoBusca:string = '';
-
-  subscription:Subscription;
-
-  livro:Livro
+  campoBusca = new FormControl()
 
   constructor(
     private livroService : LivroService
   ) { }
 
-  buscarLivros() {
-    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
-      next: (items) => {
-        this.listaLivros = this.livrosResultadoParaLivros(items)
-      },
-      error: erro => console.error(erro),
-    })
-  }
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo inicial')),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.livroService.buscar(valorDigitado)),
+    tap((retornoApi) => console.log('Requisições ao servidor',retornoApi)),
+    map((items) => this.livrosResultadoParaLivros(items))
+  )
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map (item => {
       return new LivroVolumeInfo(item);
     })
-  }
-
-  ngOnDestroy(){
-    this.subscription.unsubscribe();
   }
   
 }
